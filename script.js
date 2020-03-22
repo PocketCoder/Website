c018db25e68aa22d5214961ba05fb7e88e1656ec
PocketCoder <jake.williams77@gmail.com>
2020-03-22 15:43:07 +0000

    slider is implemented

---
+++ script.js
@@ -0,0 +1,22 @@
+jQuery.fn.isFullyVisible = function() {
+
+    var win = $(window);
+
+    var viewport = {
+        top : win.scrollTop(),
+        left : win.scrollLeft()
+    };
+
+    viewport.right = viewport.left + win.width();
+    viewport.bottom = viewport.top + win.height();
+
+    var elemtHeight = this.height();
+    elemtHeight = Math.round(elemtHeight);
+    var bounds = this.offset();
+    bounds.top = bounds.top + elemtHeight;
+    bounds.right = bounds.left + this.outerWidth();
+    bounds.bottom = bounds.top + this.outerHeight();
+
+    return (!(viewport.right < bounds.left || viewport.left > bounds.right || viewport.bottom < bounds.top || viewport.top > bounds.bottom));
+
+}
